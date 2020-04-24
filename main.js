7ac16c79110631199d591f9eda12fa968e0e841d
evan.m.reynolds95@gmail.com <evanr95@MSI.localdomain>
2020-04-24 11:00:53 -0500

    updated with second way that's dynamic

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 document.getElementById("message").addEventListener("keyup", () => {
   document.getElementById("one").textContent = event.target.value;
   document.getElementById("two").textContent = event.target.value
-})
+});
+
+
+// MORE DYNAMIC WAY
+
+/*
+
+document.querySelector("#message").addEventListener("keyup", () => {
+  
+  let boxes = document.querySelectorAll(".article--box")
+
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].textContent = event.target.value
+  }  
+}); 
+
+*/
